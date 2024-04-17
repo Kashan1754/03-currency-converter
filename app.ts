efffa666a702b47b26879fefdd04475f08e8bfda
Kashan1754 <167319612+Kashan1754@users.noreply.github.com>
2024-04-17 18:07:20 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,71 @@
+import inquirer from "inquirer";
+
+let conversion={
+ "PKR":{
+    "USD":0.0036,
+    "GBP":0.0029,
+    "PKR":1
+ },
+ "GBP":{
+    "USD":1.25,
+    "PKR":345.61,
+    "GBP":1
+ },
+ "USD":{
+    "GBP":0.80,
+    "PKR":277.60,
+    "USD":1
+ }
+
+}
+const answer:{
+from: "PKR" | "USD" | "GBP",
+to: "PKR" | "USD" | "GBP",
+
+amount:number
+}=await inquirer.prompt([
+
+    {
+        type:"list",
+        name:"from",
+        choices:["PKR","GBP","USD"],
+        message:"Select your currency...?"
+
+    },
+    {
+        type:"list",
+        name:"to",
+        choices:["PKR","GBP","USD"],
+        message:"Select your conversion currency"
+
+    },
+    {
+        type:"number",
+        name:"amount",
+        message:"Select your conversion amount"
+
+    }
+])
+
+
+const {from,to,amount}=answer;
+
+if(from&&to&&amount){
+    let result= conversion[from][to]*amount;
+    console.log(`your conversion from ${from} to ${to} is ${result}`)
+}
+else{
+    console.log("invalid inputs")
+}
+
+
+
+
+
+
+
+
+
+
+
+
